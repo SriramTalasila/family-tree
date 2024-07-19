e941b961d2069afabbf800eacd4a2b9a6116a643
SriRamTalasila <sriramprasadtalasila@gmail.com>
2024-07-19 17:26:04 +0530

    added spring boot

---
+++ family/src/test/java/online/talsaila/family/FamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package online.talsaila.family;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
